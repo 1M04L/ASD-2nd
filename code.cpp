a149c9adecf64a9b31761a200aa848fe829541ca
1M04L <mikhailulianenko@gmail.com>
2025-03-09 16:29:39 +0700

    Update code.cpp
    added body of createFileWithRandomNumbers
    added printFileContent

--- code.cpp
+++ code.cpp
@@ -3,8 +3,22 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <fstream>
 
-bool createFileWithRandomNumbers(const std::string &fileName, const int numbersCount, const int maxNumberValue);
+
+
+bool createFileWithRandomNumbers(const std::string &fileName, int numbersCount, int maxNumberValue) {
+    std::ofstream outFile(fileName);
+    if (!outFile) return false;
+
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<int> dist(0, maxNumberValue);
+
+    for (int i = 0; i < numbersCount; ++i)
+        outFile << dist(gen) << " ";
+
+    return true;
+}
 
 
 
@@ -44,6 +58,21 @@ int createAndSortFile(const std::string &fileName, int numbersCount, int maxNumb
     return 1;
 }
 
+void printFileContent(const std::string &fileName) {
+    std::ifstream inFile(fileName);
+    if (!inFile) {
+        std::cout << "cant open yr file" << std::endl;
+        return;
+    }
+
+    int num;
+    while (inFile >> num) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+
 int main() {
     std::string fileName = "file.txt";
     int numbersCount = 1000000;
@@ -55,8 +84,12 @@ int main() {
             std::cout << "Test passed." << std::endl;
         } else if (result == -1) {
             std::cout << "Test failed: can't create file." << std::endl;
+                             printFileContent(fileName);
+
         } else if (result == -2) {
             std::cout << "Test failed: file isn't sorted." << std::endl;
+                             printFileContent(fileName);
+
         }
     }
     
