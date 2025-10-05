40ef0af9b3ce72b4ec3138e8db03fbc9dca3c7ac
1M04L <mikhailulianenko@gmail.com>
2025-10-05 15:32:45 +0700

    Update interface_hash_function.h

--- interface_hash_function.h
+++ interface_hash_function.h
@@ -1,1 +1,10 @@
 
+#pragma once
+#include <cstddef>
+
+class InterfaceHashFunction {
+public:
+    virtual ~InterfaceHashFunction() = default;
+    virtual size_t hashFunction(int key, size_t tableSize, int attempt = 0) = 0;
+    virtual InterfaceHashFunction* clone() const = 0;
+};
